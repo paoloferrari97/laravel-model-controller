3b2650461dbe289823699bdd0855a7ad39e333cf
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-12 17:24:19 +0200

    Stampo a schermo tutti i dati di ogni film

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Movies</title>
 </head>
 <body>
-    @foreach ($movies as $movie)
-        {{$movie['title']}}
-    @endforeach
+    <div class="container">
+        <div class="cards">
+            <div class="card">
+                @foreach ($movies as $movie)
+                <h3>{{$movie['title']}}</h3>
+                <h4>({{$movie['original_title']}})</h4>
+                <span>Nazonalità: {{$movie['nationality']}}</span>
+                <span>Uscita nelle sale: {{$movie['date']}}</span>
+                <span>Votazione: {{$movie['vote']}}</span>
+                @endforeach
+            </div>            
+        </div>
+    </div>
 </body>
 </html>
